e3a16cffdd01ce8dca46f3dee6be8d9e0986942b
ishant1412 <ishant89572@gmail.com>
2026-03-18 10:04:59 +0530

    add cors

--- app.ts
+++ app.ts
@@ -1,5 +1,9 @@
 import express from "express";
+import cors from "cors"
 const app = express()
+
+app.use(express.json())
+app.use(cors())
 app.get("/hello",(req,res)=>{
  res.json({
     message:"hello guyzzzz"
